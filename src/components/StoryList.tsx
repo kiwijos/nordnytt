19b66ddd521d1650f2dad8e062677c007c451d40
Alex Lindqvist <allq22@student.bth.se>
2025-04-01 15:32:51 +0200

    Implement 'Load More' pagination for stories

--- src/components/StoryList.tsx
+++ src/components/StoryList.tsx
@@ -2,15 +2,24 @@
 import { Story } from "@/types"
 import StoryItem from "./StoryItem"
 import { useState } from "react"
+import { getTopStories } from "@/services/hn"
 
 
 type StoryListProps = {
   initialStories: Story[]
 }
 
-export default function StoryList({ initialStories }: StoryListProps) {
+const NUMBER_OF_STORIES_PER_LOAD = 10;
 
+export default function StoryList({ initialStories }: StoryListProps) {
+  const [offset, setOffset] = useState(NUMBER_OF_STORIES_PER_LOAD);
   const [stories, setStories] = useState<Story[]>(initialStories);
+
+  const loadMoreStories = async () => {
+    const newStories = await getTopStories(offset, NUMBER_OF_STORIES_PER_LOAD);
+    setStories((prev) => [...prev, ...newStories]);
+    setOffset((prev) => prev + NUMBER_OF_STORIES_PER_LOAD);
+  }
 
   return (
     <div>
@@ -21,6 +30,12 @@ export default function StoryList({ initialStories }: StoryListProps) {
           index={index} 
         />
       ))}
+      <button
+        className="bg-slate-200 text-slate-600 p-2 text-sm rounded-md hover:bg-slate-300"
+        onClick={loadMoreStories}
+      >
+        Ladda fler inlägg
+      </button>
     </div>
   )
 }
