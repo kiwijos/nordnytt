733bac3fe2c618538c0d29bbfa3b8532d3cfa33e
Alex Lindqvist <allq22@student.bth.se>
2025-04-01 15:22:50 +0200

    Prepare state management

--- src/components/StoryList.tsx
+++ src/components/StoryList.tsx
@@ -1,14 +1,20 @@
+'use client'
 import { Story } from "@/types"
 import StoryItem from "./StoryItem"
+import { useState } from "react"
+
 
 type StoryListProps = {
   initialStories: Story[]
 }
 
 export default function StoryList({ initialStories }: StoryListProps) {
+
+  const [stories, setStories] = useState<Story[]>(initialStories);
+
   return (
     <div>
-      {initialStories.map((story, index) => (
+      {stories.map((story, index) => (
         <StoryItem 
           key={story.id}
           story={story} 
