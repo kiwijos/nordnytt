a1d87e62272c65f272d89d62a777657f8543df91
Alex Lindqvist <allq22@student.bth.se>
2025-04-01 14:40:29 +0200

    Add offset and limit parameters to getTopStories function

--- src/services/hn.ts
+++ src/services/hn.ts
@@ -1,13 +1,16 @@
 import { Comment, Story } from "@/types";
 
-export const getTopStories = async ():Promise<Story[]> => {
+export const getTopStories = async (offset: number = 0, limit: number = 10):Promise<Story[]> => {
   const topstories:number[] = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json", {
     next: {
       revalidate: 120
     }
   }).then(res => res.json());
   
-  return Promise.all(topstories.splice(0, 10).map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {
+
+  const paginatedStories = topstories.slice(offset, offset + limit);
+
+  return Promise.all(paginatedStories.map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, {
     next: {
       revalidate: 120
     }
